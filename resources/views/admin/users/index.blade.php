f3b7f0326a1cb7f3ef74122c3a20f9c9d362da3e
Md Imran Hossain <mdimran1409036@gmail.com>
2020-07-07 16:32:45 +0600

    User page working without photos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,41 @@
-<h1>its working</h1>
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>users</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>User Role</th>
+            <th>Status</th>
+            <th>Email</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active ? "Active": "Not active"}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+        </tbody>
+      </table>
+
+
+@stop
+
